0f99c49c2eea5fa73bbabb8b82e1f406ccd13c37
haroon-exouzia <haroonexouzia@gmail.com>
2025-07-16 01:08:18 +0530

    Update footer and logo components for improved theme support
    - Added dark and light logo handling in the Footer component for better theme adaptability.
    - Updated LogoBox to use distinct logo images for light and dark themes.
    - Enhanced SCSS styles for footer logos to ensure proper display based on the selected theme.

--- src/components/LogoBox.jsx
+++ src/components/LogoBox.jsx
@@ -1,18 +1,18 @@
 import logoDark from '@/assets/images/nexus_logo_no_bg.png';
-import logoLight from '@/assets/images/nexus_logo_no_bg.png';
+import logoLight from '@/assets/images/nexus_white_no_bg.png';
 import logoSm from '@/assets/images/nexus_logo_no_bg.png';
 import Image from 'next/image';
 import Link from 'next/link';
 const LogoBox = () => {
   return <div className="logo-box">
-      <Link href="/dashboards/analytics" className="logo-dark">
-        <Image width={28} height={28} src={logoSm} className="logo-sm" alt="logo sm" />
-        <Image width={118} style={{height:"50px"}} src={logoDark} className="logo-lg" alt="logo dark" />
-      </Link>
-      <Link href="/dashboards/analytics" className="logo-light">
-        <Image width={28} height={28} src={logoSm} className="logo-sm" alt="logo sm" />
-        <Image width={118} style={{height:"50px"}} src={logoLight} className="logo-lg" alt="logo light" />
-      </Link>
-    </div>;
+    <Link href="/dashboards/analytics" className="logo-dark">
+      <Image width={28} height={28} src={logoSm} className="logo-sm" alt="logo sm" />
+      <Image width={118} style={{ height: "50px", width: "118px" }} src={logoDark} className="logo-lg" alt="logo dark" />
+    </Link>
+    <Link href="/dashboards/analytics" className="logo-light">
+      <Image width={28} height={28} src={logoSm} className="logo-sm" alt="logo sm" />
+      <Image width={118} style={{ height: "52px", width: "118px" }} src={logoLight} className="logo-lg" alt="logo light" />
+    </Link>
+  </div>;
 };
 export default LogoBox;
--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -2,19 +2,33 @@ import React from 'react';
 import IconifyIcon from '../wrappers/IconifyIcon';
 import { Col, Container, Row } from 'react-bootstrap';
 import Link from 'next/link';
+import Image from 'next/image';
 import { currentYear } from '@/context/constants';
 const Footer = () => {
   return <footer className="footer">
-      <Container fluid>
-        <Row>
-          <Col xs={12} className="text-center">
-            {currentYear} © Lahomes. Crafted by <IconifyIcon icon="solar:hearts-bold-duotone" className="fs-18 align-middle text-danger" />{' '}
-            <Link href="" className="fw-bold footer-text" target="_blank">
-              Techzaa
-            </Link>
-          </Col>
-        </Row>
-      </Container>
-    </footer>;
+    <Container fluid>
+      <Row className='d-flex justify-content-center align-items-center'>
+        <Col xs={12} className="text-center d-flex justify-content-center align-items-center">
+          {currentYear} © Nexus Built. Crafted by{' '}
+          {/* Light theme logo */}
+          <Image
+            src="/Flashyminds-TM-White.png"
+            height={25}
+            width={100}
+            className=" logo-light ms-1"
+            alt="Flashyminds Logo Light"
+          />
+          {/* Dark theme logo */}
+          <Image
+            src="/Flashyminds-TM.png"
+            height={25}
+            width={100}
+            className=" logo-dark ms-1"
+            alt="Flashyminds Logo Dark"
+          />
+        </Col>
+      </Row>
+    </Container>
+  </footer>;
 };
 export default Footer;
